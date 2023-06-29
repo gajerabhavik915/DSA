d93712a9bb6efc32bb131da9826cce52da3f5bc3
Bhavikkumar Gajera <113390208+gajerabhavik915@users.noreply.github.com>
2023-06-29 12:53:17 -0400

    Update Count Complete Tree Nodes.py

--- Tree Traversal/Count Complete Tree Nodes.py
+++ Tree Traversal/Count Complete Tree Nodes.py
@@ -1,1 +1,25 @@
 
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def countNodes(self, root: [TreeNode]) -> int:
+        if root is None:
+            return 0
+        count = 0
+        queue = []
+
+        queue.append(root)
+        while queue:
+            node = queue.pop(0)
+            count += 1
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+
+        print(count)
+        return count
