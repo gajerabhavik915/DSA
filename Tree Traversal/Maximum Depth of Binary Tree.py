a8b9158cc9843764ed743fcb130872424e6a48aa
Bhavikkumar Gajera <113390208+gajerabhavik915@users.noreply.github.com>
2023-07-01 17:43:48 -0400

    Update Maximum Depth of Binary Tree.py
    adding code

--- Tree Traversal/Maximum Depth of Binary Tree.py
+++ Tree Traversal/Maximum Depth of Binary Tree.py
@@ -1,1 +1,28 @@
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        queue = [root]
+        
+        maxdepth = 0
+        while queue:
+            len_queue = len(queue)
+            temp = []
+            for _ in range(len_queue):
+                node = queue.pop(0)
+                if node:
+                    temp.append(node)
+                    if node.left:
+                        queue.append(node.left)
+                    if node.right:
+                        queue.append(node.right)
+            if temp:
+                maxdepth += 1
+        return maxdepth
+
+
