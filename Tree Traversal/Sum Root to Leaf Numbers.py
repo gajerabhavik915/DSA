253876608dfcc7a59cd40f0dbba32529adf33e5d
Bhavikkumar Gajera <113390208+gajerabhavik915@users.noreply.github.com>
2023-07-03 11:34:15 -0400

    Update Sum Root to Leaf Numbers.py

--- Tree Traversal/Sum Root to Leaf Numbers.py
+++ Tree Traversal/Sum Root to Leaf Numbers.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        ans = 0
+        def dfs(curr=None,path=''):
+            nonlocal ans
+            if curr:
+                path += str(curr.val)
+                dfs(curr.left,path)
+                dfs(curr.right,path)
+                if curr.left == None and curr.right == None:ans += int(path)
+            return
+        dfs(root)
+        return ans
