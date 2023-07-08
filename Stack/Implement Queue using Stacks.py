c92d0260639f880adf22286ec892be25ed0a8954
Bhavikkumar Gajera <113390208+gajerabhavik915@users.noreply.github.com>
2023-07-08 18:01:27 -0400

    Update Implement Queue using Stacks.py
    Adding second method

--- Stack/Implement Queue using Stacks.py
+++ Stack/Implement Queue using Stacks.py
@@ -42,3 +42,47 @@ class MyQueue:
 # param_3 = obj.peek()
 # param_4 = obj.empty()
 
+
+# method 2
+
+"""
+In this method, created two queue, then load all existing element of Queue 1 to Queue 2 first. Then in the empty 
+Queue 1 and then loaded new element to Queue 1 and again after reload all previous element to Queue 1 from Queue 2
+
+"""
+
+class MyQueue:
+
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
+
+    def push(self, x: int) -> None:
+        # self.s2.append(x)
+        # len_s1 = len(self.s1)
+        # while self.s1:
+        #     self.s2.append(self.s1.pop(0))
+        #
+        # self.s1, self.s2 = self.s2, self.s1
+        while self.s1:
+            self.s2.append(self.s1.pop())
+        self.s1.append(x)
+        while self.s2:
+            self.s1.append(self.s2.pop())
+
+    def pop(self) -> int:
+        # self.s1.pop()
+        return self.s1.pop()
+
+    def peek(self) -> int:
+        return self.s1[-1]
+
+    def empty(self) -> bool:
+        return False if self.s1 else True
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
