f828139115bf25c551bf0c0aa1ea372cf23d7f31
Bhavikkumar Gajera <113390208+gajerabhavik915@users.noreply.github.com>
2023-07-12 19:04:22 -0400

    Update Iterative Method to find Height of Binary Tree.py
    Adding code

--- Queue/Iterative Method to find Height of Binary Tree.py
+++ Queue/Iterative Method to find Height of Binary Tree.py
@@ -1,1 +1,32 @@
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# method-1 : by returning length of result list
+
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        result = []
+        que = collections.deque()
+        que.append(root)
+
+        while que:
+            len_que = len(que)
+            append_list = []
+            for i in range(len_que):
+                node = que.popleft()
+                if node:
+                    append_list.append(node.val)
+                    que.append(node.left)
+                    que.append(node.right)
+            if append_list:
+                result.append(append_list)
+        print(len(result))
+        return result
+
+
